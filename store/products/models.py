73e34aea0dfec94cf8d7522ddf30dfeee35bb7e5
Rom1xa <abakumoff.roman2018@yandex.ru>
2024-08-30 20:51:59 +0300

    models and db

--- store/products/models.py
+++ store/products/models.py
@@ -1,1 +1,21 @@
 from django.db import models
+
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):  # type: ignore
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+    image = models.ImageField(upload_to="products_images")
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):  # type: ignore
+        return f"{self.category}: {self.name}"
